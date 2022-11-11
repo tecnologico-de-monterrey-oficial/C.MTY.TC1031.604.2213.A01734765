881b7c33ad546576e4e2afa8205a52b9f1dc1a6a
ernestogithub1 <102172191+ernestogithub1@users.noreply.github.com>
2022-11-11 01:31:32 -0600

    Changes to be committed: 	new file:   ADTs/06-Heap/ejemploHeap.cpp
     Untracked files:
    	ADTs/06-Heap/Heap.h

---
+++ ADTs/06-Heap/ejemploHeap.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "Heap.h"
+
+template<class T>
+void HeapSortD(vector<T>& list) {
+    Heap<T> heap(list); 
+    list.clear();
+    while (!heap.isEmpty()) {
+        list.push_back(heap.getTop());
+        heap.pop();
+    }
+}
+
+template<class T>
+void HeapSortA(vector<T>& list) {
+    MinHeap<T> heap(list); 
+    list.clear();
+    while (!heap.isEmpty()) {
+        list.push_back(heap.getTop());
+        heap.pop();
+    }
+}
+
+int main()
+{
+    
+    //vector<int> list = {8,5,17,19,21,28};
+    Heap<int> heap(8,5,17,19,21,28);
+
+    heap.print();
+
+    return 0;
+}
